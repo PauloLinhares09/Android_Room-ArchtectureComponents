2f15b72580515d7f8455ced348f94eb1c3840e67
Paulo Linhares <plinhares9@gmail.com>
2018-09-14 13:04:06 -0300

    layout

--- app/src/main/java/br/com/packapps/androidroom_arc/MainActivity.kt
+++ app/src/main/java/br/com/packapps/androidroom_arc/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +16,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+
+    }
+
+
+    fun btSeeTeams(view: View){
+
+    }
+
+    fun btSeePlayers(view: View){
+
+    }
+
+    fun btSeeJustPlayersOfFlamengo(view: View){
 
     }
 
